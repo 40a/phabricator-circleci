59d06f1f71ec6ddc1f78b7a0cb557bea0f4f1552
Jack Lindamood <jack@signalfx.com>
2015-11-08 20:35:45 -0800

    Project does not use godeps

--- circle_check.sh
+++ circle_check.sh
@@ -4,11 +4,11 @@ set -ex
 pushd "$SRC_PATH"
 
 if [ -z "$CIRCLE_ARTIFACTS" ]; then
-	export CIRCLE_ARTIFACTS=/tmp
+  export CIRCLE_ARTIFACTS=/tmp
 fi
 
 mdl --warnings < README.md
 
-env GOPATH="$GOPATH:$(godep path)" gobuild -verbose -verbosefile "$CIRCLE_ARTIFACTS/gobuildout.txt"
+gobuild -verbose -verbosefile "$CIRCLE_ARTIFACTS/gobuildout.txt"
 CGO_ENABLED=0 go build -v -installsuffix .
 popd
