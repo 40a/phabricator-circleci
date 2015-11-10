f7efe6ff043db64fe1797f29fdcb6c7028aeb327
Jack Lindamood <jack@signalfx.com>
2015-11-10 08:31:43 -0800

    Fix skip count

--- circleci.go
+++ circleci.go
@@ -87,8 +87,8 @@ func (g *circleCiMsg) diffIds() (int64, int64) {
 }
 
 var diffResultTemplate = template.Must(template.New("").Parse(
-	`| Build Result | Build time | Test count | Failing tests | Passing tests | Skipped Tests | Build Number
-| ------------ | ---------- |  --------- | ------------- | ------------- | ------------
+`| Build Result | Build time | Test count | Failing tests | Passing tests | Skipped Tests | Build Number
+| ------------- | ---------- | ---------- | ------------- | ------------- | ------------  | ------------
 | {{ .BuildResult }} | {{ .BuildTime }} | {{ .TestCount }}  | {{ .FailingTests }} | {{ .PassingTests }} | {{ .SkippedTests }} | {{ .BuildNumber }}
 
 {{ if .Tests | len }}
@@ -166,7 +166,7 @@ func (g *circleCiMsg) populateTestResults(ctx context.Context) (diffResultStruct
 				tr.Result = unitPass
 				s.PassingTests++
 			}
-			if circleTestResult.Result == "success" {
+			if circleTestResult.Result == "skip" {
 				tr.Result = unitSkip
 				s.SkippedTests++
 			}
